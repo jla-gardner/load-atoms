21089215f90dd6ae210f5039c34435587f752913
John Gardner <gardner.john97+dphil@gmail.com>
2023-02-16 18:43:36 +0000

    adding primitive tests for validation

---
+++ tests/test_validation.py
@@ -0,0 +1,54 @@
+import pytest
+
+from load_atoms.validation import (
+    In,
+    IsBibTeX,
+    ListOf,
+    OneOf,
+    Optional,
+    Required,
+    Validator,
+)
+
+bob = {
+    "name": "bob",
+    "description": "a happy little fellow",
+    "age": 42,
+    "hobbies": ["coding", "reading", "writing"],
+}
+
+
+def test_required():
+    true_rules = [
+        Required("name"),  # exists
+        Required("description", str),  # correct type
+        Required("age", int),  # correct type
+        Required("hobbies", ListOf(str)),  # correct type
+        Required("age", lambda x: x > 0),  # custom validation
+    ]
+
+    false_rules = [
+        Required("made_up_field"),  # doesn't exist
+        Required("name", int),  # wrong type
+    ]
+
+    # test each rule individually
+    for rule in true_rules:
+        assert rule(bob), f"rule {rule} failed"
+
+    for rule in false_rules:
+        assert not rule(bob), f"rule {rule} should have failed"
+
+    # test all rules together
+    schema = Validator(*true_rules)
+    schema.validate(bob)
+
+
+def test_failure():
+    rule = Required("name", int)
+    assert not rule(bob)
+
+    validator = Validator(rule)
+    # assert that validation fails
+    with pytest.raises(ValueError):
+        validator.validate(bob)
