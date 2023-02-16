c255d242f7248637d7e63d994df1d816df9a9fd4
John Gardner <gardner.john97+dphil@gmail.com>
2023-02-16 13:01:39 +0000

    tidy up GAP17 dataset

--- dev/rebuild_all_docs.py
+++ dev/rebuild_all_docs.py
@@ -1,8 +1,10 @@
-from generate_page import build_datasets_index, build_page
+from generate_page import DOC_SOURCE, build_datasets_index, build_page
 
 from load_atoms.database import DATASETS
 
 if __name__ == "__main__":
+    for file in (DOC_SOURCE / "datasets").glob("*.rst"):
+        file.unlink()
     for dataset_id in DATASETS:
         build_page(dataset_id)
     build_datasets_index()
--- docs/source/datasets.rst
+++ docs/source/datasets.rst
@@ -1,2 +1,2 @@
+.. include:: datasets/C-GAP-17-dimers.rst
 .. include:: datasets/C-GAP-17-train.rst
-.. include:: datasets/C-dimers.rst
