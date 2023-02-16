c94921de2d602d36a2a649643ac02abcbdbe4ec8
John Gardner <gardner.john97+dphil@gmail.com>
2023-02-16 12:33:30 +0000

    improving docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'load-atoms'
-copyright = '2023, John Gardner'
-author = 'John Gardner'
-release = '0.0.1'
+project = "load-atoms"
+copyright = "2023, John Gardner"
+author = "John Gardner"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -21,14 +21,24 @@ extensions = [
     "nbsphinx",
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
+    "sphinx_copybutton",
 ]
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
+html_logo = "logo.svg"
+html_theme_options = {
+    "sidebar_hide_name": True,
+}
+
+
+# Other stuff
+
+copybutton_prompt_text = r">>> |\.\.\. |\$ "
+copybutton_prompt_is_regexp = True
