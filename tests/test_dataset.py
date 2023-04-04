1c9f2523472f35e1574e3a13c1afd3b9cbf16b05
John Gardner <gardner.john97+dphil@gmail.com>
2023-04-04 14:22:02 +0100

    more tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -7,6 +7,7 @@ from load_atoms.dataset import Dataset, summarise_dataset
 
 STRUCTURES = [Atoms("H2O"), Atoms("H2O2")]
 
+
 def _is_water_dataset(ds):
     assert len(ds) == 2
     assert set(ds[0].symbols) == {"H", "O"}
@@ -30,23 +31,26 @@ def test_dataset_writeable_and_readable(tmp_path):
 
 def test_slice():
     ds = dataset(STRUCTURES)
-    
+
     structure = ds[0]
     assert isinstance(structure, Atoms), "Indexing should return an Atoms object"
     assert structure is STRUCTURES[0], "Indexing should return the same structure"
 
     sub_dataset = ds[1:]
     assert isinstance(sub_dataset, Dataset), "Slicing should return a dataset"
-    assert len(sub_dataset) == 1, "Slicing should return the correct number of structures"
+    assert (
+        len(sub_dataset) == 1
+    ), "Slicing should return the correct number of structures"
 
 
-def test_can_load_from_id():    
-    # pass root to avoid downloading the dataset
-    structures = dataset("QM7", root="src/load_atoms/datasets") 
+def test_can_load_from_id():
+    # pass root to avoid downloading the dataset
+    structures = dataset("QM7", root="src/load_atoms/datasets")
     assert len(structures) == 7165
 
     with pytest.raises(ValueError, match="is not known."):
         dataset("made_up_dataset")
+
 
 def test_summarise():
     ds = dataset(STRUCTURES)
@@ -56,4 +60,11 @@ def test_summarise():
     ds = dataset("C-GAP-17-train", root="src/load_atoms/datasets")
     summary = summarise_dataset(ds)
     assert "energy" in summary, "The summary should contain the property names"
-    
+
+
+def test_useful_error_message():
+    with pytest.raises(
+        ValueError,
+        match="Please provide a string, a list of structures, or a path to a file.",
+    ):
+        dataset(1)
