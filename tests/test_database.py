6b3d61bebe3948b9ddb104bf85f8a13ed8e965a6
John Gardner <gardner.john97+dphil@gmail.com>
2023-04-05 09:09:40 +0100

    move to using pydantic

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,40 +1,33 @@
 from pathlib import Path
 
 import pytest
-import yaml
+from pydantic import ValidationError
 
 from load_atoms.checksums import generate_checksum
 from load_atoms.database import (
-    _DESCRIPTOR_FILES,
     DATASETS,
-    DESCRIPTION_BLUEPRINT,
+    DatasetDescription,
+    find_all_descriptor_files,
     get_description_of,
 )
 
 _local_path_to_datasets = Path(__file__).parent.parent / "src/load_atoms/datasets"
 
 
-@pytest.mark.parametrize("file", _DESCRIPTOR_FILES, ids=lambda x: x.name)
-def test_description_file(file):
-    """
-    test that all dataset description files are valid
-    """
-    with open(file) as f:
-        DESCRIPTION_BLUEPRINT.validate(yaml.safe_load(f))
-
-
 def test_all_loaded():
     """
     test that all datasets are loaded
     """
-    assert len(DATASETS) == len(_DESCRIPTOR_FILES)
+
+    assert len(DATASETS) == len(find_all_descriptor_files())
 
 
 @pytest.mark.parametrize("dataset", DATASETS.values(), ids=lambda x: x.name)
 def test_checksums(dataset):
     """
-    test that all dataset checksums are valid
+    test that all dataset checksums are correct
     """
+
     for path, checksum in dataset.files.items():
         path = _local_path_to_datasets / path
         assert checksum == generate_checksum(path)
@@ -43,3 +36,48 @@ def test_checksums(dataset):
 def test_description():
     desc = get_description_of("C-GAP-17-train")
     assert desc.name == "C-GAP-17-train"
+
+
+def get_correct_dict():
+    return DATASETS["C-GAP-17-train"].dict()
+
+
+def test_incorrect_name():
+    kwargs = get_correct_dict()
+    kwargs["name"] = dict(_not="a string")
+
+    with pytest.raises(ValidationError):
+        DatasetDescription(**kwargs)
+
+
+def test_incorrect_license():
+    kwargs = get_correct_dict()
+    kwargs["license"] = "Not a real license"
+
+    with pytest.raises(ValidationError):
+        DatasetDescription(**kwargs)
+
+
+def test_incorrect_files():
+    kwargs = get_correct_dict()
+
+    # files should be a mapping of str to checksum
+
+    # not a mapping
+    kwargs["files"] = 1
+    with pytest.raises(ValidationError):
+        DatasetDescription(**kwargs)
+
+    # not a mapping of str to checksum
+    kwargs["files"] = {1: 1}
+    with pytest.raises(ValidationError):
+        DatasetDescription(**kwargs)
+
+
+def test_incorrect_citation():
+    kwargs = get_correct_dict()
+
+    # citation should be a bibtex string
+    kwargs["citation"] = "this is not a bibtex string"
+    with pytest.raises(ValidationError):
+        DatasetDescription(**kwargs)
