dda4fa77c643647cf5b1f77b05e2a68259c4c5a5
John Gardner <gardner.john97+dphil@gmail.com>
2023-04-05 09:09:40 +0100

    move to using pydantic

--- src/load_atoms/checksums.py
+++ src/load_atoms/checksums.py
@@ -1,4 +1,5 @@
 import hashlib
+import string
 from pathlib import Path
 
 
@@ -11,3 +12,10 @@ def generate_checksum(file_path: Path) -> str:
             sha256_hash.update(byte_block)
 
     return sha256_hash.hexdigest()[:12]
+
+
+def valid_checksum(hash: str) -> bool:
+    """Check if a hash is valid."""
+    if len(hash) != 12:
+        return False
+    return all(c in string.hexdigits for c in hash)
--- src/load_atoms/database.py
+++ src/load_atoms/database.py
@@ -1,55 +1,60 @@
-import warnings
 from pathlib import Path
+from typing import Dict, List
 
 import yaml
+from pydantic import BaseModel, validator
 
+from load_atoms.checksums import valid_checksum
 from load_atoms.util import DATASETS_DIR
-from load_atoms.validation import (
-    BibTeX,
-    Blueprint,
-    FileHash,
-    IsIn,
-    Mapping,
-    Optional,
-    Required,
-)
-
-valid_licences = ["MIT", "CC-BY-4.0", "CC BY-NC-SA 4.0"]
-
-DESCRIPTION_BLUEPRINT = Blueprint(
-    # -----------------------
-    # --- required fields ---
-    # -----------------------
-    Required("name", str),
-    Required("description", str),
-    # test that files is a mapping of files that exist to their hashes
-    Required("files", Mapping(str, FileHash())),
-    # -----------------------
-    # --- optional fields ---
-    # -----------------------
-    Optional("citation", BibTeX()),
-    Optional("license", IsIn(valid_licences)),
-    Optional("representative_structures", [int]),
-    Optional("long_description", str),
-    Optional("per_atom_properties", dict),
-    Optional("per_structure_properties", dict),
-)
 
 
-class DatasetDescription(DESCRIPTION_BLUEPRINT.dataclass()):
+def is_bibtex(data: str) -> bool:
+    data = data.strip()
+    return data.startswith("@") and data.endswith("}")
+
+
+class DatasetDescription(BaseModel):
+    name: str
+    description: str
+    files: Dict[str, str]
+    citation: str = None
+    license: str = None
+    representative_structures: List[int] = None
+    long_description: str = None
+    per_atom_properties: dict = None
+    per_structure_properties: dict = None
+
+    @validator("license")
+    def validate_license(cls, v):
+        valid_licences = ["MIT", "CC-BY-4.0", "CC BY-NC-SA 4.0"]
+        if v not in valid_licences:
+            raise ValueError(f"Invalid license: {v}. Must be one of {valid_licences}")
+        return v
+
+    @validator("files")
+    def validate_files(cls, v):
+        for data in v.values():
+            if not valid_checksum(data):
+                raise ValueError(f"Invalid checksum: {data}")
+        return v
+
+    @validator("citation")
+    def validate_citation(cls, v):
+        if not is_bibtex(v):
+            raise ValueError(f"Invalid BibTeX: {v}")
+        return v
+
     @classmethod
     def from_file(cls, path: Path) -> "DatasetDescription":
         with open(path) as f:
             data = yaml.safe_load(f)
-
         return cls(**data)
 
 
-_DESCRIPTOR_FILES = list(DATASETS_DIR.glob("**/*.yaml"))
-DATASETS = {}
-for file in _DESCRIPTOR_FILES:
-    entry = DatasetDescription.from_file(file)
-    DATASETS[entry.name] = entry
+def find_all_descriptor_files() -> List[Path]:
+    """Find all descriptor files."""
+
+    return list(DATASETS_DIR.glob("**/*.yaml"))
 
 
 def is_known_dataset(dataset_id: str) -> bool:
@@ -61,3 +66,9 @@ def get_description_of(dataset_id: str) -> DatasetDescription:
     """Get the database entry for a dataset."""
     assert is_known_dataset(dataset_id), f"Dataset {dataset_id} is not known."
     return DATASETS[dataset_id]
+
+
+DATASETS = {}
+for file in find_all_descriptor_files():
+    entry = DatasetDescription.from_file(file)
+    DATASETS[entry.name] = entry
