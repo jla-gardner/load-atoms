e8205675e4e6d21adc2c369d103f46622cc9692c
John Gardner <gardner.john97+dphil@gmail.com>
2023-05-12 14:27:57 +0100

    progress bars

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -2,7 +2,7 @@ import pytest
 import requests
 from aiohttp import ClientConnectorError
 
-from load_atoms.backend import RequestError, download_structures, download_thing
+from load_atoms.backend import RequestError, download_file, download_structures
 from load_atoms.util import DATASETS_DIR
 
 
@@ -12,7 +12,7 @@ async def test_real_download(tmp_path):
 
     # download the file
     path = tmp_path / "test.json"
-    await download_thing(url, path)
+    await download_file(url, path)
 
     # check that the file exists
     assert path.exists(), "The file was not downloaded"
@@ -23,13 +23,13 @@ async def test_missing_download(tmp_path):
     # attempt to download a file from a non-existent url
     fake_url = "https://this.is.a.fake.url/file.json"
     with pytest.raises(ClientConnectorError):
-        await download_thing(fake_url, tmp_path / "test.json")
+        await download_file(fake_url, tmp_path / "test.json")
 
     # attempt to download a file that does not exist
     # and check that we report on the 404 error
     real_url_fake_file = "https://raw.githubusercontent.com/jla-gardner/load-atoms/main/made_up_file.json"
     with pytest.raises(RequestError, match="404"):
-        await download_thing(real_url_fake_file, tmp_path / "test.json")
+        await download_file(real_url_fake_file, tmp_path / "test.json")
 
 
 @pytest.mark.asyncio
