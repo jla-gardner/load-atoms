9ecf62e750ce016095375ddabf8d29d2d08d25cf
John Gardner <gardner.john97+dphil@gmail.com>
2023-07-28 11:26:56 +0100

    Bump version 0.0.13 -> 0.0.14

--- dev/docs/source/conf.py
+++ dev/docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "load-atoms"
 copyright = "2023, John Gardner"
 author = "John Gardner"
-release = "0.0.13"
+release = "0.0.14"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- src/load_atoms/__init__.py
+++ src/load_atoms/__init__.py
@@ -1,5 +1,5 @@
 from load_atoms.api import dataset
 from load_atoms.logic import manipulations
 
-__version__ = "0.0.13"
+__version__ = "0.0.14"
 __all__ = ["dataset", "manipulations"]
