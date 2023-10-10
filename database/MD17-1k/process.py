e0456b01a72fa11e98c8716f22ec04c4d407955c
John Gardner <gardner.john97+dphil@gmail.com>
2023-10-10 09:59:28 +0100

    update md17 to eV

--- database/MD17-1k/process.py
+++ database/MD17-1k/process.py
@@ -2,6 +2,7 @@ import numpy as np
 from ase import Atoms
 from ase.io import write
 
+kcal_per_mol_to_eV = 0.04336414
 rand = np.random.RandomState(42)
 
 systems = (
@@ -15,22 +16,22 @@ for system in systems:
     archive = np.load(f"md17/md17_{system}.npz")
     arrays = {name: archive[name] for name in archive.files}
 
-    # randomly choose 1,200 structures: 1k train, 100 val, 100 test
-    indices = rand.choice(len(arrays["E"]), size=1_200, replace=False)
+    # randomly choose 2_000 structures: 950 train, 50 val, 1k test
+    indices = rand.choice(len(arrays["E"]), size=2_000, replace=False)
 
     for i, idx in enumerate(indices):
         atoms = Atoms(
             numbers=arrays["z"],
             positions=arrays["R"][idx],
         )
-        atoms.info["energy"] = arrays["E"][idx]
+        atoms.info["energy"] = arrays["E"][idx] * kcal_per_mol_to_eV
         atoms.info["split"] = (
-            "train" if i < 1_000 else "val" if i < 1_100 else "test"
+            "train" if i < 950 else "val" if i < 1_000 else "test"
         )
         atoms.info["system"] = system.replace(
             "2017", ""
         )  # benzene2017 -> benzene
-        atoms.arrays["forces"] = arrays["F"][idx]
+        atoms.arrays["forces"] = arrays["F"][idx] * kcal_per_mol_to_eV
         all_structures.append(atoms)
 
 
