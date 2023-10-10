7eebc0383e6898ab1318dd61c704e924732a4a28
zakmachachi <zakmachachi@googlemail.com>
2023-10-10 16:04:57 +0100

    split by config

--- src/load_atoms/logic/manipulations.py
+++ src/load_atoms/logic/manipulations.py
@@ -6,7 +6,6 @@ from typing import Callable, Optional, Sequence, Tuple, Union
 
 import numpy as np
 from ase import Atoms
-
 from load_atoms.logic import Dataset
 
 __all__ = ["filter_by", "cross_validate_split"]
@@ -79,6 +78,7 @@ def cross_validate_split(
     fold,
     k=5,
     n_test: Optional[int] = None,
+    by_config_type: Optional[str] = None,
     seed: int = 0,
 ) -> Tuple[Dataset, Dataset]:
     """
@@ -95,6 +95,8 @@ def cross_validate_split(
     n_test : Optional[int], optional
         The number of structures to use for testing, by default None.
         If None, this will be set to len(dataset) // k.
+    by_config_type : Optional[str], optional
+        Whether to split the dataset evenly by config_type, by default None.
     seed : int, optional
         The random seed to use, by default 0.
 
@@ -122,12 +124,63 @@ def cross_validate_split(
     if n_test is None:
         n_test = len(dataset) // k
 
-    idxs = np.arange(len(dataset))
-    np.random.RandomState(seed).shuffle(idxs)
-    idxs = np.roll(idxs, fold * len(idxs) // k)
-    train, test = idxs[:-n_test], idxs[-n_test:]
+    if by_config_type is not None:
+        # the user needs to supply the str so we know what to split by. for example some may use 'config_type' and others may use 'config_type_1' etc..
+        # the aim of this is to ensure that we have a balanced dataset for each fold and that we have the same number of each config_type in each fold
+        # this is important for the training of the model
+        # we need to split the dataset by config_type
+
+        # get the unique config_types
+        config_types = np.unique(
+            [structure.info[by_config_type] for structure in dataset]
+        )
+
+        # get the indices of the config_types
+        config_type_idxs = [
+            np.where(
+                [structure.info[by_config_type] == config_type for structure in dataset]
+            )[0]
+            for config_type in config_types
+        ]
+
+        # we have n_test total, we should split this evenly between the config_types
+        n_test_per_config_type = n_test // len(config_types)
+
+        # Initialize an array to hold test indices
+        remainder = n_test % len(config_types)
+
+        np.random.RandomState(seed).shuffle(config_type_idxs)
+
+        test_indices = np.array([])
+
+        for i in range(len(config_types)):
+            # Determine the number of testing structures for this config_type
+            num_testing_for_config = n_test_per_config_type + (
+                1 if i < remainder else 0
+            )
+
+            # Randomly choose indices for testing without replacement
+            chosen_indices = np.random.RandomState(seed).choice(
+                config_type_idxs[i], num_testing_for_config, replace=False
+            )
+
+            # Append the chosen indices to the test_indices array
+            test_indices = np.concatenate((test_indices, chosen_indices))
+
+        # get the indices for the train set
+        train_indices = np.setdiff1d(np.arange(len(dataset)), test_indices)
+
+        # shuffle the indices
+        np.random.RandomState(seed).shuffle(train_indices)
+        np.random.RandomState(seed).shuffle(test_indices)
+
+    else:
+        idxs = np.arange(len(dataset))
+        np.random.RandomState(seed).shuffle(idxs)
+        idxs = np.roll(idxs, fold * len(idxs) // k)
+        train_indices, test_indices = idxs[:-n_test], idxs[-n_test:]
 
     return (
-        Dataset.from_structures([dataset[t] for t in train]),
-        Dataset.from_structures([dataset[t] for t in test]),
+        Dataset.from_structures([dataset[t] for t in train_indices]),
+        Dataset.from_structures([dataset[t] for t in test_indices]),
     )
