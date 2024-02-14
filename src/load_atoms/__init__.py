547f4f019344ab48f73b394116d2586a7cc1b547
John Gardner <gardner.john97+dphil@gmail.com>
2024-02-14 12:36:00 +0000

    Bump version 0.1.4 -> 0.1.5

--- src/load_atoms/__init__.py
+++ src/load_atoms/__init__.py
@@ -2,5 +2,5 @@ from . import manipulations
 from .dataset import load_dataset
 from .visualise import view
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __all__ = ["load_dataset", "manipulations", "view"]
