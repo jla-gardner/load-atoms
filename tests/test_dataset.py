d4da06370041fd7aedcbe5cdde46fa33ed1db1da
John Gardner <gardner.john97+dphil@gmail.com>
2024-02-15 16:50:22 +0000

    increase test coverage

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -11,32 +11,32 @@ from load_atoms.utils import UnknownDatasetException
 STRUCTURES = [Atoms("H2O"), Atoms("H2O2")]
 
 
-def _is_water_dataset(ds):
-    assert len(ds) == 2
-    assert set(ds[0].symbols) == {"H", "O"}
+def _is_water_dataset(dataset):
+    assert len(dataset) == 2
+    assert set(dataset[0].symbols) == {"H", "O"}
 
 
 def test_dataset_from_structures():
-    ds = load_dataset(STRUCTURES)
-    _is_water_dataset(ds)
+    dataset = load_dataset(STRUCTURES)
+    _is_water_dataset(dataset)
 
 
 def test_dataset_writeable_and_readable(tmp_path):
-    ds = load_dataset(STRUCTURES)
-    write(tmp_path / "test.xyz", ds)  # type: ignore
+    dataset = load_dataset(STRUCTURES)
+    write(tmp_path / "test.xyz", dataset)  # type: ignore
 
-    ds2 = load_dataset(read(tmp_path / "test.xyz", index=":"))  # type: ignore
-    _is_water_dataset(ds2)
+    dataset2 = load_dataset(read(tmp_path / "test.xyz", index=":"))  # type: ignore
+    _is_water_dataset(dataset2)
 
-    ds3 = load_dataset(tmp_path / "test.xyz")
-    _is_water_dataset(ds3)
+    dataset3 = load_dataset(tmp_path / "test.xyz")
+    _is_water_dataset(dataset3)
 
 
 @pytest.mark.filterwarnings("ignore:Creating a dataset with a single structure")
 def test_indexing():
-    ds = load_dataset(STRUCTURES)
+    dataset = load_dataset(STRUCTURES)
 
-    structure = ds[0]
+    structure = dataset[0]
     assert isinstance(
         structure, Atoms
     ), "Indexing should return an Atoms object"
@@ -44,7 +44,7 @@ def test_indexing():
         structure is STRUCTURES[0]
     ), "Indexing should return the same structure"
 
-    sub_dataset = ds[1:]
+    sub_dataset = dataset[1:]
     assert isinstance(
         sub_dataset, AtomsDataset
     ), "Slicing should return a dataset"
@@ -53,7 +53,7 @@ def test_indexing():
     ), "Slicing should return the correct number of structures"
 
     indices = np.array([0, 1, 0, 1])
-    sub_dataset = ds[indices]
+    sub_dataset = dataset[indices]
     assert isinstance(
         sub_dataset, AtomsDataset
     ), "Indexing should return a dataset"
@@ -62,7 +62,7 @@ def test_indexing():
     ), "Indexing should return the correct number of structures"
 
     indices = np.array([True, False])
-    sub_dataset = ds[indices]
+    sub_dataset = dataset[indices]
     assert isinstance(
         sub_dataset, AtomsDataset
     ), "Indexing should return a dataset"
@@ -71,7 +71,7 @@ def test_indexing():
     ), "Indexing should return the correct number of structures"
 
     indices = [0, 1, 1]
-    sub_dataset = ds[indices]
+    sub_dataset = dataset[indices]
     assert isinstance(
         sub_dataset, AtomsDataset
     ), "Indexing should return a dataset"
@@ -90,12 +90,12 @@ def test_can_load_from_id():
 
 
 def test_summarise():
-    ds = load_dataset(STRUCTURES)
-    summary = summarise_dataset(ds)
+    dataset = load_dataset(STRUCTURES)
+    summary = summarise_dataset(dataset)
     assert "Dataset" in summary, "The summary should contain the dataset name"
 
-    ds = load_dataset("C-GAP-17", root="./testing-datasets")
-    summary = repr(ds)
+    dataset = load_dataset("C-GAP-17", root="./testing-datasets")
+    summary = repr(dataset)
     assert "energy" in summary, "The summary should contain the property names"
 
 
@@ -125,10 +125,18 @@ def test_useful_warning(tmp_path):
 
 
 def test_info_and_arrays():
-    ds = load_dataset("QM7", root="./testing-datasets")
+    dataset = load_dataset("QM7", root="./testing-datasets")
 
-    assert "energy" in ds.info
-    assert isinstance(ds.info["energy"], np.ndarray)
+    assert "energy" in dataset.info
+    assert isinstance(dataset.info["energy"], np.ndarray)
 
-    assert "positions" in ds.arrays
-    assert ds.arrays["positions"].shape[-1] == 3
+    assert "positions" in dataset.arrays
+    assert dataset.arrays["positions"].shape[-1] == 3
+
+
+def test_properties():
+    dataset = load_dataset(STRUCTURES)
+
+    assert len(dataset) == 2
+    assert dataset.n_atoms == 7
+    assert (dataset.structure_sizes == [3, 4]).all()
---
+++ tests/test_view.py
@@ -0,0 +1,32 @@
+# testing whether a visualisation works is difficult
+# as a first stab, we can make sure that the function runs,
+# and that the utility functions are working as expected
+
+import pytest
+from ase.build import molecule
+from load_atoms import view
+from load_atoms.visualise import unique_variable_name
+
+
+@pytest.mark.parametrize("show_bonds", [True, False])
+def test_view(show_bonds):
+    ethanol = molecule("CH3CH2OH")
+    view(ethanol, show_bonds)
+
+    ethanol.center(vacuum=5)
+    ethanol.pbc = [True, True, True]
+    view(ethanol, show_bonds)
+
+
+def test_unique_variable_name():
+    # ensure that the function returns a unique name
+    # that is also a valid javascript variable name
+    names = [unique_variable_name() for _ in range(10)]
+    assert len(names) == len(set(names)), "Names should be unique"
+
+    for name in names:
+        assert name.isidentifier(), "Name should be a valid identifier"
+        assert name[0].isalpha(), "Name should start with a letter"
+        assert all(
+            c.isalnum() or c == "_" for c in name
+        ), "Name should only contain letters, numbers, or underscores"
