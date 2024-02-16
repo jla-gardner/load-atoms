3ecbd9a6ec72c7c838f3c28b2ecc78e6ef410c88
John Gardner <gardner.john97+dphil@gmail.com>
2024-02-16 14:11:56 +0000

    update documentation

--- src/load_atoms/__init__.py
+++ src/load_atoms/__init__.py
@@ -34,18 +34,31 @@ def load_dataset(
 
     >>> from load_atoms import load_dataset
     >>> dataset = load_dataset("C-GAP-17")
+    Downloading C-GAP-17.extxyz | ███████████████████████ | 100.0%
+    The C-GAP-17 dataset is covered by the CC BY-NC-SA 4.0 license.
+    Please cite the C-GAP-17 dataset if you use it in your work.
+    >>> dataset
+    C-GAP-17:
+        structures: 4,530
+        atoms: 284,965
+        species:
+            C: 100.00%
+        properties:
+            per atom: (force)
+            per structure: (config_type, detailed_ct, energy, split)
+
 
     Optionally save a dataset to an explicit root directory:
 
-    >>> dataset("C-GAP-17", root="./my-datasets")
+    >>> load_dataset("C-GAP-17", root="./my-datasets")
 
     Wrap a list of structures in a dataset:
 
-    >>> dataset([Atoms("H2O"), Atoms("H2O2")])
+    >>> load_dataset([Atoms("H2O"), Atoms("H2O2")])
 
     Load a dataset from a file:
 
-    >>> dataset("path/to/file.xyz")
+    >>> load_dataset("path/to/file.xyz")
     """
 
     if isinstance(thing, list) and all(isinstance(s, ase.Atoms) for s in thing):
