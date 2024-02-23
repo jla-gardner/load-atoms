70bb7c7def88c34d3bc73de32354e890fb5848c6
John Gardner <gardner.john97+dphil@gmail.com>
2024-02-23 14:44:48 +0000

    Bump version 0.2.6 -> 0.2.7

--- src/load_atoms/__init__.py
+++ src/load_atoms/__init__.py
@@ -8,7 +8,7 @@ from ase.io import read
 from .atoms_dataset import AtomsDataset, DescribedDataset
 from .visualisation import view
 
-__version__ = "0.2.6"
+__version__ = "0.2.7"
 __all__ = ["load_dataset", "view"]
 
 
